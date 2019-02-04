3ef87a05a2056917a05d70e1069e386ba58dce37
Ian Wallace <iankarlwallace@gmail.com>
2019-02-03 21:33:04 -0800

    Addition of reading XLSX file an then querying based on those names.

---
+++ dbtools.js
@@ -0,0 +1,43 @@
+// Functions to load new patients, list them out or get a list of patient names and DOB's to query
+'use strict';
+const XLSX = require('xlsx');
+
+var dbtools = {
+    load_new_patients: async function(xlsxFile) {
+	console.log('load_new_patients started [',xlsxFile,']');
+	var myWb = XLSX.readFile(xlsxFile);
+	var myWs = myWb.Sheets[myWb.SheetNames[0]];
+
+   	var rows = [];
+	var rowNum;
+	var range = XLSX.utils.decode_range(myWs['!ref']);
+	for(rowNum = range.s.r; rowNum <= range.e.r; rowNum++){
+		if (rowNum !== 0) { 
+			var ptNameCell = myWs[XLSX.utils.encode_cell({r: rowNum, c: 1})];
+			var mrnCell = myWs[XLSX.utils.encode_cell({r: rowNum, c: 6})];
+			var dobCell = myWs[XLSX.utils.encode_cell({r: rowNum, c: 7})];
+
+			// Confirm that DOB is set
+			if( typeof dobCell !== 'undefined' ) {
+				// The dob must be in MM/DD/YYYY format or the website won't find the search
+				delete dobCell.w;
+				dobCell.z = 'mm/dd/yyyy';
+				XLSX.utils.format_cell(dobCell);
+
+				var names = ptNameCell.w.split(';');
+				var lname = names[0].trimStart();
+				var fname = names[1].trimStart();
+
+				console.log('FOUND [',fname,'] [',lname,'] [',mrnCell.w,'] [',dobCell.w,']');
+				rows.push([fname, lname, mrnCell.w, dobCell.w]);
+			} else {
+				console.log('No DOB set for row [',rowNum,']');
+			}
+		}
+	}
+	return rows;
+	console.log('load_new_patients finished');
+    }
+};
+
+module.exports = dbtools;
--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const puppeteer = require('puppeteer');
 const credentials = require('./credentials');
 const patients = require('./patients');
 const cures_website = require('./cures_website');
+const dbtools = require('./dbtools');
+const xlsx_file = './xlsx/c_s5to11_18_c.xlsx'
 
 async function run() {
-  const browser = await puppeteer.launch({slowMo: 50, headless: false, args: ['--window-size=1920,1080']});
+  var pts = await dbtools.load_new_patients(xlsx_file);
+  console.log('Query needed on [',pts,']');
+
+  const browser = await puppeteer.launch({slowMo: 250, headless: false, args: ['--window-size=1920,1080']});
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
 
@@ -15,7 +20,15 @@ async function run() {
   let pword = await credentials.get_password();
   await cures_website.login(page, uname, pword);
   await cures_website.par(page);
-  await cures_website.par_search(page, "Ian", "Wallace", "01/01/1970");
+
+  for(let pt in pts) {
+	  let fname = pts[pt][0].substring(0,3);
+	  let lname = pts[pt][1].substring(0,3);
+	  let dob = pts[pt][3];
+	  console.log('Query for [',fname,'][',lname,'][',dob,']');
+	  await cures_website.par_search(page, fname, lname, dob);
+	  await cures_website.par(page);
+  }
 
   await page.screenshot({ path: 'downloads/cures.doj.ca.gov.png' });
 
