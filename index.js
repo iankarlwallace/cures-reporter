0afb95279e360ec975a15b6f14612886cf4ca2bb
Ian Wallace <iankarlwallace@gmail.com>
2019-02-13 22:11:56 -0800

    Updates that use yargs to process arguments.

--- index.js
+++ index.js
@@ -3,18 +3,49 @@
 
 const appRoot = require('app-root-path');
 const puppeteer = require('puppeteer');
+const util = require('util');
 
-const mLog = require(appRoot + '/lib/logging-func');
+const mLog = require(appRoot + '/lib/logging-func')(__filename);
 const credentials = require(appRoot + '/lib/credentials');
-const patients = require(appRoot + '/lib/patients');
 const cures_website = require(appRoot + '/lib/cures_website');
 const dbtools = require(appRoot + '/lib/dbtools');
 
-const xlsx_file = appRoot + '/xlsx/c_s5to11_18_c.xlsx'
+const xlsx_file = appRoot + '/xlsx/c_s5to11_18_c.xlsx';
+const downloads_dir = appRoot + './downloads';
 
-async function run() {
-  var pts = await dbtools.load_new_patients(xlsx_file);
+var argv = require('yargs')
+  .usage('Usage: $0 <command[> [options]')
+  .command(['query', '$0'],
+    'Query the CURES website',
+    () => {}, (argv) => {
+      query(argv.file, argv.downloadsDir).catch((e) => mLog.error(e));
+    })
+  .command('process',
+    'Process downloaded files in download dir',
+    () => {}, (argv) => {
+      process().catch((e) => mLog.error(e));
+    })
+  .demandCommand(1)
+  .example('$0 query -f patient_data_.xlsx', 'Query patient data from xlsx file')
+  .example('$0 process', 'Try to process all downloaded files from')
+  .alias('f', 'file')
+  .alias('d', 'downloadsDir')
+  .default('f', xlsx_file)
+  .default('d', downloads_dir)
+  .epilog('copyright 2019')
+  .argv;
 
+async function process() {
+  mLog.info('Stubbed process files function');
+  return;
+}
+
+async function query(myFile, myDir) {
+  mLog.info('Starting to work on query [%s] with dir [%s]', myFile, myDir);
+  var pts = await dbtools.load_new_patients(myFile);
+
+  // Currently the downloads_dir is hardcoded in the cures_website module
+  // cures_website.downloads_dir = myDir;
   cures_website.cleanup_downloads_dir();
 
   let uname = await credentials.get_username();
@@ -23,38 +54,40 @@ async function run() {
   let ptCount = pts.length;
   let ptHits = 0;
   let ptMiss = 0;
-  mLog.info('Working on queries on [',ptCount,'] patients');
 
-  for(let pt in pts) {
-	mLog.info('Query for patient [',pt,']');
-	let fname = pts[pt][0];
-	let lname = pts[pt][1];
-	let dob = pts[pt][3];
+  for (let pt in pts) {
+    let fname = pts[pt][0];
+    let lname = pts[pt][1];
+    let dob = pts[pt][3];
 
- 	if ( cures_website.confirm_pt_file_needed(fname, lname, dob) ) {
+    if (cures_website.confirm_pt_file_needed(fname, lname, dob)) {
 
-		const browser = await puppeteer.launch({headless: true,
-			slowMo: 50,
-			args: ['--window-size=960,1080']});
-		const page = await browser.newPage();
-		await page.setViewport({ width: 960, height: 1080 });
+      const browser = await puppeteer.launch({
+        headless: true,
+        slowMo: 50,
+        args: ['--window-size=960,1080']
+      });
+      const page = await browser.newPage();
+      await page.setViewport({
+        width: 960,
+        height: 1080
+      });
 
-		await cures_website.login(page, uname, pword);
-		await cures_website.par(page);
+      await cures_website.login(page, uname, pword);
+      await cures_website.par(page);
 
-		let return_searches = await cures_website.par_search(page, fname, lname, dob);
-		ptHits = ptHits + return_searches[0];
-		ptMiss = ptMiss + return_searches[1];
+      let return_searches = await cures_website.par_search(page, fname, lname, dob);
+      ptHits = ptHits + return_searches[0];
+      ptMiss = ptMiss + return_searches[1];
 
-		await cures_website.logout(page);
-		await page.close();
-		await browser.close();
-	} else {
-		mLog.info('Pt query not needed for [',fname,'] [',lname,'] [',dob,']');
-	}
+      await cures_website.logout(page);
+      await page.close();
+      await browser.close();
+    } else {
+      mLog.info('Pt query not needed for [%s] [%s] [%s] [%s]', pt, fname, lname, dob);
+    }
   }
 
   let ptTot = ptHits + ptMiss;
-  mLog.info('Total patient searches [',ptTot,'] hits [',ptHits,'] miss [',ptMiss,']');
+  mLog.info('Pt searches [%s] hits [%s] miss [%s]', ptTot, ptHits, ptMiss);
 }
-run().catch((e) => mLog.error(e));
